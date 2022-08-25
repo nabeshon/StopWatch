5e47a76fafb58b8ce212cbda27560415f2569c77
ShoWatanabe <nabesho017@gmail.com>
2022-08-25 20:59:57 +0900

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by 渡邉昇 on 2022/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StopWatchUITests/StopWatchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StopWatchUITestsLaunchTests.swift
+//  StopWatchUITests
+//
+//  Created by 渡邉昇 on 2022/08/25.
+//
+
+import XCTest
+
+class StopWatchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
